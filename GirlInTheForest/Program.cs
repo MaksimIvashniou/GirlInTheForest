4ab16405aba9f7b3621dbac1a7dac5d618a285a4
MIvashniou <maksim.ivashniou@gmail.com>
2021-12-01 14:11:12 +0300

    Add project files.

---
+++ GirlInTheForest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GirlInTheForest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
